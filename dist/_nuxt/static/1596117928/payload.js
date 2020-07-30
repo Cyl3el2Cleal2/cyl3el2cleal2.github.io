2a00100670b49fb8e6113bbc6fd90bac22c9fcfd
cyl3el2cleal2 <nathachian@gmail.com>
2020-07-30 21:06:15 +0700

    Update

---
+++ dist/_nuxt/static/1596117928/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", (function(a,b){return {data:[{boxColClass:"flex flex-col bg-gray-200",boxRowClass:"flex justify-center bg-gray-200",linkData:[[{title:a,link:b},{title:a,link:b}],[{title:a,link:b}],[{title:a,link:b}]]}],fetch:[],mutations:void 0}}("youtube","https:www.youtube.com")));
